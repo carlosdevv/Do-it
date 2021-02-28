eb66e84770e5a0e838ea009528a4a63812237507
carlosdevv <carloslopessf@gmail.com>
2021-02-27 22:05:13 -0300

    [Feat] Done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,67 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { ChallengeBox } from '../components/ChallengeBox';
 
-import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
-      <ExperienceBar />
-      <Head>
-        <title>Inicio | moveit</title>
-      </Head>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <ExperienceBar />
+        <Head>
+          <title>DO-IT</title>
+        </Head>
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
 
-    </div>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+//manipular quias dados sao repassados da camada do next pra camada front-end(react).
+//acessar uma API atraves desse metodo o next irá executar, caso seja no metodo acima (Home)--
+//--ele nao executa e os motores de busca tbm nao.
+//Ou seja, chamadas API para buscar informações e serem mostradas em tela utilizar no metodo--
+//--getServerSideProps e depois repassar as infos na funcão React (Home).
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    }
+  }
 }
